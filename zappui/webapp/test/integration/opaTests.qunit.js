474ed11824dcf8fb385928203025d3b3b74ac2ba
gokulmanickamworks <gokulmanickamworks18@gmail.com>
2024-11-01 20:03:57 +0000

    Initial Commit

---
+++ zappui/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["zappui/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ zappui/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zappui/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
